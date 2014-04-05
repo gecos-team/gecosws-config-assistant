23f5e99ffe9ccbe553be2da76c7ed80c1246506e
David Amian <damian@emergya.com>
2014-04-05 14:23:57 +0200

    Chaged ActiveDirectoryConf with new attrbutes from JSON

--- firstboot/serverconf/ActiveDirectoryConf.py
+++ firstboot/serverconf/ActiveDirectoryConf.py
@@ -22,59 +22,113 @@ __license__ = "GPL-2"
 
 import firstboot.validation as validation
 
+class ActiveDirectoryProperties():
+    def __init__(self):
+        self._data = {}
+        self._data['fqdn'] = ''
+        self._data['workgroup'] = ''
+        self._data['sssd_conf'] = ''
+        self._data['krb5_conf'] = ''
+        self._data['smb_conf'] = ''
+        self._data['pam_conf'] = ''
+
+    def load_data(self, conf, specific):
+        msg = 'ServerConf: Key "%s" not found in the configuration file.'
+        if not specific:
+            try:
+                self.set_fqdn(conf['fqdn'])
+            except KeyError as e:
+                print msg % ('fqdn',)
+            try:
+                self.set_workgroup(conf['workgroup'])
+            except KeyError as e:
+                print msg % ('workgroup',)
+            
+        else:
+            try:
+                self.set_sssd_conf(conf['sssd_conf'])
+            except KeyError as e:
+                print msg % ('sssd_conf',)
+            try:
+                self.set_krb5_conf(conf['krb5_conf'])
+            except KeyError as e:
+                print msg % ('krb5_conf',)
+            try:
+                self.set_smb_conf(conf['smb_conf'])
+            except KeyError as e:
+                print msg % ('smb_conf',)
+            try:
+                self.set_pam_conf(conf['pam_conf'])
+            except KeyError as e:
+                print msg % ('pam_conf',)
+
+    def get_fqdn(self):
+        return self._data['fqdn'].encode('utf-8')
+    
+    def set_fqdn(self, fqdn):
+        self._data['fqdn'] = fqdn
+        return self
+
+    def get_workgroup(self):
+        return self._data['workgroup'].encode('utf-8')
+
+    def set_workgroup(self, workgroup):
+        self._data['workgroup'] = workgroup
+        return self
+
+    def get_sssd_conf(self):
+        return self._data['sssd_conf']
+
+    def set_sssd_conf(self, sssd_conf):
+        self._data['sssd_conf'] = sssd_conf
+        return self
+
+    def get_krb5_conf(self):
+        return self._data['krb5_conf']
+
+    def set_krb5_conf(self, krb5_conf):
+        self._data['krb5_conf'] = krb5_conf
+        return self
+
+    def get_smb_conf(self):
+        return self._data['smb_conf']
+
+    def set_smb_conf(self, smb_conf):
+        self._data['smb_conf'] = smb_conf
+        return self
+
+
 
 class ActiveDirectoryConf():
 
     def __init__(self):
         self._data = {}
-        self._data['fqdn'] = ''
-        self._data['dns_domain'] = ''
-        self._data['user'] = ''
-        self._data['passwd'] = ''
+        self._data['specific_conf'] = ''
+        self._ad_properties = ActiveDirectoryProperties()
 
     def load_data(self, conf):
         msg = 'ServerConf: Key "%s" not found in the configuration file.'
         try:
-            self.set_fqdn(conf['fqdn'])
+            self.set_fqdn(conf['specific_conf'])
         except KeyError as e:
-            print msg % ('fqdn',)
+            print msg % ('specific_conf',)
         try:
-            self.set_dns_domain(conf['dns'])
+            self.set_ad_properites.load_data(conf['auth_properties'])
         except KeyError as e:
-            print msg % ('dns',)
+            print msg % ('auth_properties',)
 
     def validate(self):
-        valid = not validation.is_empty(self._data['fqdn']) \
-            and not validation.is_empty(self._data['dns_domain'])
-        return valid
+        return True
 
-    def get_fqdn(self):
-        return self._data['fqdn'].encode('utf-8')
+    def get_specific_conf(self):
+        return self._data['specific_conf'].encode('utf-8')
 
-    def set_fqdn(self, fqdn):
-        self._data['fqdn'] = fqdn
+    def set_specific_conf(self, specific_conf):
+        self._data['specific_conf'] = specific_conf
         return self
 
-    def get_dns_domain(self):
-        return self._data['dns_domain'].encode('utf-8')
-
-    def set_dns_domain(self, dns_domain):
-        self._data['dns_domain'] = dns_domain
-        return self
-
-    def get_user(self):
-        return self._data['user'].encode('utf-8')
-
-    def set_user(self, user):
-        self._data['user'] = user
-        return self
-
-    def get_passwd(self):
-        return self._data['passwd'].encode('utf-8')
-
-    def set_passwd(self, passwd):
-        self._data['passwd'] = passwd
-        return self
+    def get_auth_properties(self):
+        return self._ad_properties
 
     def __str__(self):
         return str(self._data)
