c478e8e475319eab3d2886ab7685d11aac570716
David Amian <damian@emergya.com>
2014-04-17 13:53:57 +0200

    Changed method to control local user

--- firstboot/pages/localUsers/__init__.py
+++ firstboot/pages/localUsers/__init__.py
@@ -22,6 +22,7 @@ __license__ = "GPL-2"
 
 
 import pwd
+import grp
 import os, Dialogs
 from gi.repository import Gtk
 import firstboot.pages
@@ -53,8 +54,8 @@ class LocalUsersPage(PageWindow.PageWindow):
     def load_page(self, params=None):
      
         self.emit('status-changed', 'localUsers', not __REQUIRED__)
-        content = serverconf.get_json_content()
-        self.serverconf = serverconf.get_server_conf(content)
+        self.serverconf = serverconf.get_server_conf(None)
+
         self.init_treeview()
         self.reload_page()
 
@@ -120,60 +121,70 @@ workstation. \n\nUsers authenticated by external services (AD, LDAP...) director
         self.ui.tvUsers.set_enable_search(True)
         self.ui.tvUsers.set_search_column(1)
         self.ui.tvUsers.set_show_expanders(False)
+#        select = self.ui.tvUsers.get_selection()
+#        select.connect("cursor-changed", self.on_tvUsersCursorChanged)
 
-    def _render_user_column(self, column, cell, model, iter, userdata):
-        user = model.get_value(iter, 0)
+    def _render_user_column(self, column, cell, model, iterat, userdata):
+        user = model.get_value(iterat, 0)
         property = 'text'
         text = user['login']
         cell.set_property(property, text)
 
+    def add_user_treeview(self, user):
+        store = self.ui.tvUsers.get_model()
+        userdict = {
+            'login': user.get_user(),
+            'uid': '',
+            'gid': '',
+            'name': user.get_name(),
+            'home': '',
+            'shell': '',
+            'groups': '',
+            'is_admin': False
+        }
+        store.append([userdict])
+        self.ui.tvUsers.set_model(store)
+
+    def delete_user_treeview(self, user):
+        store = self.ui.tvUsers.get_model()
+        store.remove(store.get_iter(self._selected_path))
+        self.ui.tvUsers.set_model(store)
+
+    def update_user_treeview(self, user):
+        store = self.ui.tvUsers.get_model()
+        for row in store:
+            if row[0]['login'] == user.get_user():
+                userdict = row[0]
+                userdict['name'] = user.get_name()
+        self.ui.tvUsers.set_model(store)
+
     def load_users(self):
-        #read users to delete, to avoid to show them in the widget
-        userstodelete = []
-        for user in self.serverconf.get_users_conf().get_users_list():
-            if user.get_actiontorun() == 'delete':
-                userstodelete.append(user.get_user())
-        
-        #load users in the system, excluding those to be deleted
-        #and load changes in parameters
         users = self.read_users()
         store = self.ui.tvUsers.get_model()
         store.clear()
         
         for user in users:
-            if user['login'] not in userstodelete:
-                for user2 in self.serverconf.get_users_conf().get_users_list():
-                    if user2.get_user() == user['login']:
-                        user['name'] = user2.get_name()
-                store.append([user])
-        
-        #load users to create
-        for user in self.serverconf.get_users_conf().get_users_list():
-            if user.get_actiontorun() == 'create':
-                userdict = {
-                    'login': user.get_user(),
-                    'uid': '',
-                    'gid': '',
-                    'name': user.get_name(),
-                    'home': '',
-                    'shell': '',
-                    'groups': '',
-                    'is_admin': False,
-                    }
-                store.append([userdict])
-        
+            store.append([user])
+
         self.ui.tvUsers.set_model(store)
 
     def _select_user(self):
+        if not hasattr(self, '_selected_path'):
+            self._selected_path = '0'
         self.ui.tvUsers.get_selection().select_path(self._selected_path)
         self.on_tvUsersCursorChanged(self.ui.tvUsers)
 
     def on_tvUsersCursorChanged(self, widget):
         #if self._active_user and self._active_user['updated'] == True:
         #    return
-        store, iter = widget.get_selected()
-        user = store.get_value(iter, 0)
-        self._selected_path = store.get_path(iter)
+        if type(widget) is Gtk.TreeView:
+            return
+        store, iterat = widget.get_selected()
+        if iterat == None:
+            self._selected_path = '0'
+        else:
+            self._selected_path = store.get_path(iterat)
+        user = store.get_value(iterat, 0)
         self.set_active_user(user)
 
     def set_active_user(self, user):
@@ -221,37 +232,32 @@ workstation. \n\nUsers authenticated by external services (AD, LDAP...) director
             changeuser.set_actiontorun('modify')
             changeuser.set_user(user['login'])
             changeuser.set_name(user['name'])
-            changeuser.set_password(user['password'])
+            if update_passwd:
+                changeuser.set_password(user['password'])
             changeuser.add_group(user['groups'])
             
             #avoid to add 2 or more modify entries for the same user
             #add only the last one
             #if there is a create operation, rewrite the create with the modified parameters
             userlist = []
-            modifiedbefore = False
-            for user2 in self.serverconf.get_users_conf().get_users_list():
+            userinserver = None
+            users_conf = self.serverconf.get_users_conf()
+            for user2 in users_conf.get_users_list():
                 if user2.get_user() == changeuser.get_user():
-                    if user2.get_actiontorun() == 'create':
-                        modifiedbefore = True
-                        #rewrite the create operation with the new parameters
-                        user2.set_name(changeuser.get_name())
-                        user2.set_password(changeuser.get_password())
-                        user2.add_groups(changeuser.get_groups())
-                        userlist.append(user2)
-                    elif user2.get_actiontorun() == 'modify':
-                        modifiedbefore = True
-                        userlist.append(changeuser) #add the new changes
-                    elif user2.get_actiontorun() == 'delete':
-                        userlist.append(user2)
-                else:
-                    userlist.append(user2)
-            
-            if not modifiedbefore:
-                userlist.append(changeuser)
-            
-            self.serverconf.get_users_conf().clear()
-            self.serverconf.get_users_conf().add_users_to_list(userlist)
-            self.reload_page()
+                    userinserver = user2
+                    users_conf.remove_user_from_list(user2)
+                    break
+            if userinserver != None:
+                userinserver.set_name(changeuser.get_name())
+                userinserver.set_password(changeuser.get_password())
+                userinserver.clear_groups()
+                userinserver.add_groups(changeuser.get_groups())
+                if userinserver.get_actiontorun() == 'delete':
+                    userinserver.set_actiontorun('modify')
+                changeuser = userinserver
+                
+            users_conf.add_user_to_list(changeuser)
+            self.update_user_treeview(changeuser)
             self._select_user()
 
         except LocalUsersException as e:
@@ -268,21 +274,27 @@ workstation. \n\nUsers authenticated by external services (AD, LDAP...) director
     def on_btnAddClicked(self, widget):
         login_info = Dialogs.new_user_dialog()
 
+
         if login_info == False:
             # Pressed 'Cancel' or dialog closed
             return
 
         if not self.validate_user(login_info):
             return
-        
+        users_conf = self.serverconf.get_users_conf()
+
         #checking for duplicated users
         usedusers = set()
+        createdusers = {}
+        removeusers = []
         for user in self.read_users():
             usedusers.add(user['login'])
-        for user in self.serverconf.get_users_conf().get_users_list():
+            createdusers[user['login']] = user
+        for user in users_conf.get_users_list():
             if user.get_actiontorun() == 'create':
                 usedusers.add(user.get_user())
             elif user.get_actiontorun() == 'delete':
+                removeusers.append(user)
                 usedusers.remove(user.get_user())
         if login_info['login'] in usedusers:
             Dialogs.user_error_dialog(_('Duplicated user'))
@@ -295,9 +307,16 @@ workstation. \n\nUsers authenticated by external services (AD, LDAP...) director
             newuser.set_user(login_info['login'])
             newuser.set_name(login_info['name'])
             newuser.set_password(login_info['password'])
-            newuser.add_group('') #groups not added
-            self.serverconf.get_users_conf().add_user_to_list(newuser)
-            self.reload_page()
+            for user in removeusers:
+                if newuser.get_user() == user.get_user():
+                    if newuser.get_user() in createdusers.keys():
+                        newuser.set_actiontorun('modify')
+                        groups = createdusers[newuser.get_user()]['groups'].split(' ')
+                        newuser.add_groups(groups)
+                    users_conf.remove_user_from_list(user)
+            users_conf.add_user_to_list(newuser)
+
+            self.add_user_treeview(newuser)
             self._select_user()
             
 
@@ -317,21 +336,24 @@ workstation. \n\nUsers authenticated by external services (AD, LDAP...) director
             deleteuser.set_deletehome(action[1])
             
             #remove any operation on the user we are going to delete
+            users_conf = self.serverconf.get_users_conf()
             userlist = []
-            createdhere = False
-            for user2 in self.serverconf.get_users_conf().get_users_list():
-                if user2.get_user() != deleteuser.get_user(): #operation on a different user, ok
-                    userlist.append(user2)
-                elif user2.get_actiontorun() == 'create': #if user was created in this session, then, do not create+delete
-                    createdhere = True
+            createdUser = False
+            for user in users_conf.get_users_list():
+                #operation on a different user, ok
+                if user.get_user() == deleteuser.get_user(): 
+                    if user.get_actiontorun() == 'create':
+                        users_conf.remove_user_from_list(user)
+                        createdUser = True
+                        break
+                    else:
+                        users_conf.remove_user_from_list(user)
+                        break
             
-            if not createdhere:
-                userlist.append(deleteuser)
+            if not createdUser:
+                users_conf.add_user_to_list(deleteuser)
             
-            self.serverconf.get_users_conf().clear()
-            self.serverconf.get_users_conf().add_users_to_list(userlist)
-            
-            self.reload_page()
+            self.delete_user_treeview(deleteuser)
             self._select_user()
 
         except LocalUsersException as e:
@@ -361,36 +383,31 @@ workstation. \n\nUsers authenticated by external services (AD, LDAP...) director
         return valid
 
     def read_users(self, min_uid=1000):
-
-        users = []
-
-        f = open('/etc/passwd')
         try:
-            for line in f:
-                line = line.strip()
-                tokens = line.split(':')
-                if int(tokens[2]) >= min_uid and tokens[0] != 'nobody':
-                    cmd = 'groups %s' % (tokens[0],)
-                    pid, exit_code, output = self._run_command(cmd)
-                    groups = output.split(':')
-                    groups = groups[1].strip()
-
-                    user = {
-                        'login': tokens[0],
-                        'uid': tokens[2],
-                        'gid': tokens[3],
-                        'name': tokens[4].split(',')[0],
-                        'home': tokens[5],
-                        'shell': tokens[6],
-                        'groups': groups,
-                        'is_admin': ('sudo' in groups.split(' '))
-                    }
-                    users.append(user)
-
-        finally:
-            f.close()
-        
-        return users
+            users = []
+            for user in pwd.getpwall():
+                if user.pw_uid < min_uid or user.pw_name == 'nobody':
+                    continue
+                groups = []
+                groups.append(grp.getgrnam(user[0])[0])
+                for group in grp.getgrall():
+                    if user[0] in group[3]:
+                        groups.append(group[0])
+                userstore = {
+                    'login': user.pw_name,
+                    'uid': user.pw_uid,
+                    'gid': user.pw_gid,
+                    'name': user.pw_gecos.split(',')[0],
+                    'home': user.pw_dir,
+                    'shell': user.pw_shell,
+                    'groups': str.join(' ', groups),
+                    'is_admin': ('sudo' in groups)
+                }
+                users.append(userstore)
+            
+            return users
+        except Exception as e:
+            raise LocalUsersException(e)
 
 
     def _run_command(self, cmd):
