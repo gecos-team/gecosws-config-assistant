1641613342efa402da4e6052b8abfebc80323a2f
David Amian <damian@emergya.com>
2014-04-28 23:21:21 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_datafiles(datadir):
     return datafiles
 
 datafiles = get_datafiles('data')
-datafiles.append(('share/applications/', glob.glob('data/gecos-config-assistant.desktop'))
+datafiles.append(('share/applications/', glob.glob('data/gecos-config-assistant.desktop')))
 
 def update_config(values={}):
 
