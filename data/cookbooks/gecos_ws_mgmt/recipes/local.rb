e5148f8af8e4050bf235acc691b5b6148f6f2ce3
David Amian <damian@emergya.com>
2014-04-29 19:00:04 +0200

    Added gcc call and comment sssd

--- data/cookbooks/gecos_ws_mgmt/recipes/local.rb
+++ data/cookbooks/gecos_ws_mgmt/recipes/local.rb
@@ -8,7 +8,12 @@
 # All rights reserved - EUPL License V 1.1
 # http://www.osor.eu/eupl
 #
-include_recipe "gecos_ws_mgmt::sssd_mgmt"
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+#include_recipe "gecos_ws_mgmt::sssd_mgmt"
+
 
 gecos_ws_mgmt_tz_date 'localtime' do
   server node[:gecos_ws_mgmt][:misc_mgmt][:tz_date_res][:server]
@@ -22,5 +27,15 @@ gecos_ws_mgmt_chef node[:gecos_ws_mgmt][:misc_mgmt][:chef_conf_res][:chef_server
   chef_node_name node[:gecos_ws_mgmt][:misc_mgmt][:chef_conf_res][:chef_node_name]
   chef_admin_name node[:gecos_ws_mgmt][:misc_mgmt][:chef_conf_res][:chef_admin_name]
   job_ids node[:gecos_ws_mgmt][:misc_mgmt][:chef_conf_res][:job_ids]
+  action  :setup
+end
+
+gecos_ws_mgmt_gcc node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:uri_gcc] do
+  gcc_link node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:gcc_link]
+  gcc_nodename node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:gcc_nodename]
+  gcc_username node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:gcc_username]
+  gcc_pwd_user node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:gcc_pwd_user]
+  gcc_selected_ou node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:gcc_selected_ou]
+  job_ids node[:gecos_ws_mgmt][:misc_mgmt][:gcc_res][:job_ids]
   action  :setup
 end
