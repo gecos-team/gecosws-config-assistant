d553e3f7d772e6b086288e6a8e117b00824c6cb0
David Amian <damian@emergya.com>
2014-05-14 16:59:48 +0200

    Update cookbook ohai-gecos

--- data/cookbooks/ohai-gecos/files/default/plugins/pclabel.rb
+++ data/cookbooks/ohai-gecos/files/default/plugins/pclabel.rb
@@ -1,3 +1,15 @@
-provides 'pclabel'
+provides 'ohai_gecos'
 
-pclabel from("cat /etc/pclabel")
+if ohai_gecos.nil?
+  ohai_gecos Mash.new
+end
+begin
+  pclabel = ''
+  File.open('/etc/pclabel','r') do |pclabelfile|
+    pclabel = pclabelfile.gets
+  end
+rescue Exception => e
+  puts e.message
+end
+
+ohai_gecos['pclabel'] = pclabel
