7009dfc416b49a37853d1e474e98d05bc90c7737
David Amian <damian@emergya.com>
2014-05-22 13:55:01 +0200

    Fix gcc delete request

--- data/cookbooks/gecos_ws_mgmt/providers/gcc.rb
+++ data/cookbooks/gecos_ws_mgmt/providers/gcc.rb
@@ -66,8 +66,8 @@ action :setup do
       if not new_resource.uri_gcc.nil? and not new_resource.gcc_nodename.nil? and not new_resource.gcc_username.nil? and not new_resource.gcc_pwd_user.nil? and not new_resource.gcc_selected_ou.nil?
         Chef::Log.info("GCC: Desenlazando cliente de GECOS Control Center")
         begin
-          resource = RestClient::Resource.new(new_resource.uri_gcc + '/register/computer/', :user => new_resource.gcc_username, :password => new_resource.gcc_pwd_user)
-          response = resource.delete :node_id => new_resource.gcc_nodename, :content_type => :json, :accept => :json
+          resource = RestClient::Resource.new(new_resource.uri_gcc + '/register/computer/?node_id=' + new_resource.gcc_nodename, :user => new_resource.gcc_username, :password => new_resource.gcc_pwd_user)
+          response = resource.delete
           if not response.code.between?(200,299)
             Chef::Log.error('The GCC URI not response')  
           else
