5e24a3b23bd1d6e5550acbd8fddea64edd6c95c6
David Amian <damian@emergya.com>
2014-06-25 16:07:34 +0200

    Added provider Upstart for services

--- data/cookbooks/gecos_ws_mgmt/providers/chef.rb
+++ data/cookbooks/gecos_ws_mgmt/providers/chef.rb
@@ -53,6 +53,7 @@ action :setup do
        
           Chef::Log.info("Activando servicio chef-client")
           service 'chef-client' do
+            provider Chef::Provider::Service::Upstart
             supports :status => true, :restart => true, :reload => true
             action [:enable, :start]
           end
@@ -110,6 +111,7 @@ action :setup do
 
         Chef::Log.info("Desactivando servicio chef-client")
         service 'chef-client' do
+          provider Chef::Provider::Service::Upstart
           supports :status => true, :restart => true, :reload => true
           action [:disable, :stop]
         end
--- data/cookbooks/gecos_ws_mgmt/providers/sssd.rb
+++ data/cookbooks/gecos_ws_mgmt/providers/sssd.rb
@@ -120,6 +120,7 @@ action :setup do
         end
   
         service 'sssd' do
+          provider Chef::Provider::Service::Upstart
           supports :status => true, :restart => true, :reload => true
           action [:enable, :start]
         end
@@ -130,6 +131,7 @@ action :setup do
     else
       Chef::Log.info("SSSD desactivado")
       service 'sssd' do
+        provider Chef::Provider::Service::Upstart
         supports :status => true, :restart => true, :reload => true
         action [:stop, :disable]
       end
