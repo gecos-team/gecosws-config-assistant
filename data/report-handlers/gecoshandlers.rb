8e64030a71ea148f3b64fe764b1b92a57b6d4f8f
David Amian <damian@emergya.com>
2014-09-08 11:49:25 +0200

    Fix report handler to solve possible error to communicate status jobs if the network config has changed

--- data/report-handlers/gecoshandlers.rb
+++ data/report-handlers/gecoshandlers.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'rest_client'
 require 'chef/log'
+require "resolv"
 
 module GECOSReports
   class StatusHandler < Chef::Handler
@@ -15,15 +16,34 @@ module GECOSReports
           gcc_control = JSON.load(f)
         end
         begin
-          resource = RestClient::Resource.new(gcc_control['uri_gcc'] + '/chef/status/')
-          response = resource.put :node_id => gcc_control['gcc_nodename'], :gcc_username => gcc_control['gcc_username']
-          if not response.code.between?(200,299)
-            Chef::Log.error('The GCC URI not response')
-          else
-            response_json = JSON.load(response.to_str)
-            if not response_json['ok']
-              Chef::Log.error(response_json['message'])
+          has_conection = false
+          tries = 0
+          dns_resolver = Resolv::DNS.new()
+          while not has_conection and tries < 10
+            begin
+              domain = gcc_control['uri_gcc'].split(':')[1].split('/')[2]
+              dns_resolver.getaddress(domain)
+              has_conection = true
+            rescue Resolv::ResolvError => e
+              sleep(1)
+              tries=tries+1
+              Chef::Log.info(tries)
             end
+          end
+          if has_conection
+            Chef::Log.info(gcc_control)
+            resource = RestClient::Resource.new(gcc_control['uri_gcc'] + '/chef/status/')
+            response = resource.put :node_id => gcc_control['gcc_nodename'], :gcc_username => gcc_control['gcc_username']
+            if not response.code.between?(200,299)
+              Chef::Log.error('The GCC URI not response')
+            else
+              response_json = JSON.load(response.to_str)
+              if not response_json['ok']
+                Chef::Log.error(response_json['message'])
+              end
+            end
+          else
+            Chef::Log.error('there is no connectivity')
           end
         rescue Exception => e
           Chef::Log.error(e.message)
