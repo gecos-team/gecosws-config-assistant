be06c33e9c08df9d9144d85de2ffa7e924ad86f4
Alfonso E.M <alfonso@el-magnifico.org>
2015-09-11 12:48:59 +0200

    Syncing recipes with gecos-workstations-management-cookbook

--- data/cookbooks/gecos_ws_mgmt/recipes/default.rb
+++ data/cookbooks/gecos_ws_mgmt/recipes/default.rb
@@ -9,18 +9,13 @@
 # http://www.osor.eu/eupl
 #
 
+# Glogal variable $gecos_os created to reduce calls to external programs
+$gecos_os = `lsb_release -d`.split(":")[1].chomp().lstrip()
+
 execute "gecos-chef-snitch" do
   command "gecosws-chef-snitch-client --set-active true"
   action :nothing
 end.run_action(:run)
-
-Chef::Log.info("Disable chef-client daemon to use wrapper")
-
-service 'chef-client' do
-  provider Chef::Provider::Service::Upstart
-  supports :status => true, :restart => true, :reload => true
-  action [:disable, :stop]
-end
 
 Chef::Log.info("Installing wrapper")
 cookbook_file "chef-client-wrapper" do
@@ -31,25 +26,21 @@ cookbook_file "chef-client-wrapper" do
   action :nothing
 end.run_action(:create_if_missing)
 
-Chef::Log.info("Enable chef-client-wrapper")
+Chef::Log.info("Enabling GECOS Agent in cron")
+  
+cron "GECOS Agent"
+    minute '30'
+    command '/usr/bin/chef-client-wrapper'
+    action :create
+end
 
-file "/var/spool/cron/crontabs/root" do
-  owner 'root'
-  group 'root'
-  action :nothing
-end.run_action(:create_if_missing)
+Chef::Log.info("Disabling old chef-client service")
 
-bash "Added cron line for wrapper" do 
-  user "root"
-  cwd "/var/spool/cron/crontabs/"
-  code <<-EOF
-    grep chef-client-wrapper root
-    if [[ $? -eq 1 ]]; then
-       echo "*/30 * * * * chef-client-wrapper" >> root
-    fi
-  EOF
-  not_if "grep chef-client-wrapper root"
-end.run_action(:run)
+service 'chef-client' do
+    provider Chef::Provider::Service::Upstart
+    supports :status => true, :restart => true, :reload => true
+    action [:disable, :stop]
+end
 
 include_recipe "gecos_ws_mgmt::software_mgmt"
 include_recipe "gecos_ws_mgmt::misc_mgmt"
